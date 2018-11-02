ee00bfddb6058e392905f67e905f2fd7f33b5a4c
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-11-01 14:57:40 -1000

    Making notifications work

--- send-notification.sh
+++ send-notification.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# We need to disable IPV6 otherwise this won't work
+# From https://askubuntu.com/a/38468
+
+sudo sh -c 'echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf'
 wget --post-data "token=acMLorGZk7ZsWPQ3KcBwHHVr4tADtX&user=u8pxz9cSkbD9VJ5j6jTRiABjix5qF8&sound=bike&message=$1" -O - https://api.pushover.net/1/messages
