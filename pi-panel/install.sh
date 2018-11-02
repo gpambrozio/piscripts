c6152858f2191f8738cca9e351cf4e494114d894
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-11-01 16:05:11 -1000

    Creating a new folder and making sure notifications are sent.

--- pi-panel/install.sh
+++ pi-panel/install.sh
@@ -3,21 +3,23 @@
 cd /home/pi
 wget https://raw.githubusercontent.com/gpambrozio/piscripts/master/send-notification.sh
 chmod +x send-notification.sh
-./send-notification.sh "Setup started"
+/home/pi/send-notification.sh "Setup started"
 
 sudo apt-get update && sudo apt-get install python2.7-dev python-pillow -y
 
-./send-notification.sh "Setup 1 of 3"
+/home/pi/send-notification.sh "Setup 1 of 3"
 
 git clone https://github.com/hzeller/rpi-rgb-led-matrix/
-./send-notification.sh "Setup 2 of 3"
+/home/pi/send-notification.sh "Setup 2 of 3"
 cd rpi-rgb-led-matrix/
 make
-./send-notification.sh "Setup 3 of 3"
+/home/pi/send-notification.sh "Setup 3 of 3"
 make build-python
 sudo make install-python
 
 cd /home/pi
+mkdir scripts
+cd scripts
 
 wget https://raw.githubusercontent.com/gpambrozio/piscripts/master/pi-panel/samplebase.py
 wget https://raw.githubusercontent.com/gpambrozio/piscripts/master/pi-panel/runtext.py
@@ -25,6 +27,7 @@ wget https://raw.githubusercontent.com/gpambrozio/piscripts/master/pi-panel/runt
 wget https://raw.githubusercontent.com/gpambrozio/piscripts/master/pi-panel/image-scroller.py
 wget https://raw.githubusercontent.com/gpambrozio/piscripts/master/pi-panel/9x15.bdf
 chmod +x runtext.py
+chmod +x image-scroller.py
 
-./send-notification.sh "Done. Rebooting."
-rm send-notification.sh
+/home/pi/send-notification.sh "Done. Rebooting."
+rm /home/pi/send-notification.sh
