30d3f1ac80d90f437f3b7a49d0e14f6c7a901672
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-11-02 08:11:05 -1000

    Adding more notifications and trying to notify on errors

--- pi-panel/install.sh
+++ pi-panel/install.sh
@@ -3,17 +3,19 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+/home/pi/send-notification.sh "Setup 1 of 4"
+
 sudo apt-get install python2.7-dev python-pillow -y
 
-/home/pi/send-notification.sh "Setup 1 of 3"
+/home/pi/send-notification.sh "Setup 2 of 4"
 
 cd /home/pi
 
 git clone https://github.com/hzeller/rpi-rgb-led-matrix/
-/home/pi/send-notification.sh "Setup 2 of 3"
+/home/pi/send-notification.sh "Setup 3 of 4"
 cd rpi-rgb-led-matrix/
 make
 
-/home/pi/send-notification.sh "Setup 3 of 3"
+/home/pi/send-notification.sh "Setup 4 of 4"
 make build-python
 sudo make install-python
