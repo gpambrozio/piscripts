212b4d014aa4274e9960d3403fcb0916aedcc5af
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-11-04 21:07:21 -1000

    Script to keep files synchronized while developing

---
+++ remotesync.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z "$1" || -z "$2" ]] ; then
+	echo "Usage: $0 machine-name machine-ip"
+	exit 1
+fi
+
+listcommand="ls -lRT $1"
+
+while true ; do
+   newfilelist=$( $listcommand )
+   if [[ $oldfilelist != $newfilelist ]] ; then
+      oldfilelist=$newfilelist
+      rsync --exclude='.*' --delete --recursive --perms --itemize-changes --times --compress --human-readable --rsh=ssh "$1/" "pi@$2:/home/pi/$1/"
+   fi
+   sleep 1 || exit 2 
+done
