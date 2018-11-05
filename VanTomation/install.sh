97a5402c519e000b49abf4010a6f760b6d8243f3
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-11-05 09:21:23 -1000

    Trying to catch errors

--- VanTomation/install.sh
+++ VanTomation/install.sh
@@ -3,6 +3,17 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+didError () {
+    errcode=$? # save the exit code as the first thing done in the trap function
+    echo "error $errorcode"
+    echo "the command executing at the time of the error was"
+    echo "$BASH_COMMAND"
+    echo "on line ${BASH_LINENO[0]}"
+	/home/pi/send-notification.sh "Error happend during install"
+    exit $errcode  # or use some other value or do return instead
+}
+trap didError ERR
+
 cd /home/pi
 
 # Bluepy: https://github.com/IanHarvey/bluepy
