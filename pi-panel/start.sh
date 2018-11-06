e37c7d0ef0a16ae68246f3f2f44b117b497f8384
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-11-05 21:11:41 -1000

    Slightly more clear start

--- pi-panel/start.sh
+++ pi-panel/start.sh
@@ -2,8 +2,8 @@
 
 SESSION_NAME="pi-panel"
 
-screen -d -m -S $SESSION_NAME
-screen -S $SESSION_NAME -p 0 -X stuff 'cd /home/pi/pi-panel && sudo ./panelserver.py
-'
 
-screen -S $SESSION_NAME -X screen 1
+screen -S $SESSION_NAME -t myWinName0 -A -d -m
+screen -S $SESSION_NAME -X screen -t myWinName1
+
+screen -S $SESSION_NAME -p myWinName1 -X stuff $'cd /home/pi/pi-panel && sudo ./panelserver.py\n'
