b946bf9fc5f951cb7a909f8289914809f1a0cd23
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-11-07 14:51:19 -1000

    Better start scripts

--- VanTomation/start.sh
+++ VanTomation/start.sh
@@ -2,12 +2,11 @@
 
 SESSION_NAME="van"
 
-screen -d -m -S $SESSION_NAME
-screen -S $SESSION_NAME -p 0 -X stuff 'cd /home/pi/VanTomation && sudo ./vantomation.py
-'
+screen -S $SESSION_NAME -t myWinName0 -A -d -m
+screen -S $SESSION_NAME -X screen -t myWinName1
 
-screen -S $SESSION_NAME -X screen 1
-screen -S $SESSION_NAME -p 1 -X stuff 'cd /home/pi/VanTomation && sudo BLENO_HCI_DEVICE_ID=1 node peripheral.js
-'
+screen -S $SESSION_NAME -p myWinName0 -X stuff $'cd /home/pi/VanTomation && sudo ./vantomation.py\n'
 
-screen -S $SESSION_NAME -X screen 2
+# Wait a bit to start the second script as it kinda depends on the first
+sleep 5
+screen -S $SESSION_NAME -p myWinName1 -X stuff $'cd /home/pi/VanTomation && sudo BLENO_HCI_DEVICE_ID=1 node peripheral.js\n'
--- pi-panel/start.sh
+++ pi-panel/start.sh
@@ -4,6 +4,5 @@ SESSION_NAME="pi-panel"
 
 
 screen -S $SESSION_NAME -t myWinName0 -A -d -m
-screen -S $SESSION_NAME -X screen -t myWinName1
 
-screen -S $SESSION_NAME -p myWinName1 -X stuff $'cd /home/pi/pi-panel && sudo ./panelserver.py\n'
+screen -S $SESSION_NAME -p myWinName0 -X stuff $'cd /home/pi/pi-panel && sudo ./panelserver.py\n'
