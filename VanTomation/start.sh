8546115aa89d89e32e0fdf54c95f14dfb29110b4
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-11-09 09:56:29 -1000

    Actually fixed the start script

--- VanTomation/start.sh
+++ VanTomation/start.sh
@@ -2,11 +2,13 @@
 
 SESSION_NAME="van"
 
+sleep 5
+
 screen -S $SESSION_NAME -t myWinName0 -A -d -m
 screen -S $SESSION_NAME -X screen -t myWinName1
 
 screen -S $SESSION_NAME -p myWinName0 -X stuff $'cd /home/pi/VanTomation && sudo ./vantomation.py\n'
 
 # Wait a bit to start the second script as it kinda depends on the first
-sleep 5
+sleep 20
 screen -S $SESSION_NAME -p myWinName1 -X stuff $'cd /home/pi/VanTomation && sudo BLENO_HCI_DEVICE_ID=1 node peripheral.js\n'
