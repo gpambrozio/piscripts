b22ee9985ffd6398b7c10912338b630b12885272
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-11-20 11:26:48 -1000

    Better naming

--- VanTomation/server/controller_manager.py
+++ VanTomation/server/controller_manager.py
@@ -9,23 +9,23 @@ class ControllerManager(DeviceManager):
 
     def __init__(self):
         SERVICE_UUID = '12345678-1234-5678-1234-56789abc0010'
-        COMMAND_CHAR_UUID = '12345679-1234-5678-1234-56789abc0010'
-        DEVICES_CHAR_UUID = '1234567a-1234-5678-1234-56789abc0010'
+        INPUT_CHAR_UUID = '12345679-1234-5678-1234-56789abc0010'
+        OUTPUT_CHAR_UUID = '1234567a-1234-5678-1234-56789abc0010'
 
-        DeviceManager.__init__(self, [[SERVICE_UUID, COMMAND_CHAR_UUID, DEVICES_CHAR_UUID]], ControllerThread)
+        DeviceManager.__init__(self, [[SERVICE_UUID, INPUT_CHAR_UUID, OUTPUT_CHAR_UUID]], ControllerThread)
         
 
 class ControllerThread(DeviceThread):
 
     def before_thread(self):
         service_uuid = self.service_and_char_uuids[0][0]
-        self.command_characteristic = self.characteristics[service_uuid][0]
-        self.devices_characteristic = self.characteristics[service_uuid][1]
-        self.start_notifications(self.command_characteristic)
+        self.input_characteristic = self.characteristics[service_uuid][0]
+        self.output_characteristic = self.characteristics[service_uuid][1]
+        self.start_notifications(self.input_characteristic)
 
 
     def received_data(self, cHandle, data):
-        if cHandle == self.command_characteristic.getHandle():
+        if cHandle == self.input_characteristic.getHandle():
             destination = data[0]
             if destination == "L":
                 strip = data[1]
@@ -42,14 +42,14 @@ class ControllerThread(DeviceThread):
 
     def broadcast_received(self, broadcast):
         if broadcast.destination == None and broadcast.prop == "Devices":
-            self.add_command(lambda: self.devices_characteristic.write("Dv" + broadcast.value))
+            self.add_command(lambda: self.output_characteristic.write("Dv" + broadcast.value))
         elif broadcast.destination == None and broadcast.prop == "Temperature" and broadcast.source == "Thermostat":
-            self.add_command(lambda: self.devices_characteristic.write("Ti%.0f" % (broadcast.value * 10)))
+            self.add_command(lambda: self.output_characteristic.write("Ti%.0f" % (broadcast.value * 10)))
         elif broadcast.destination == None and broadcast.prop == "Temperature" and broadcast.source == "AgnesOutside":
-            self.add_command(lambda: self.devices_characteristic.write("To%.0f" % (broadcast.value * 10)))
+            self.add_command(lambda: self.output_characteristic.write("To%.0f" % (broadcast.value * 10)))
         elif broadcast.destination == None and broadcast.prop == "Humidity" and broadcast.source == "Thermostat":
-            self.add_command(lambda: self.devices_characteristic.write("Hm%.0f" % (broadcast.value * 10)))
+            self.add_command(lambda: self.output_characteristic.write("Hm%.0f" % (broadcast.value * 10)))
         elif broadcast.destination == None and broadcast.prop == "On" and broadcast.source == "Thermostat":
-            self.add_command(lambda: self.devices_characteristic.write("To%d" % broadcast.value))
+            self.add_command(lambda: self.output_characteristic.write("To%d" % broadcast.value))
         elif broadcast.destination == None and broadcast.prop == "Target" and broadcast.source == "Thermostat":
-            self.add_command(lambda: self.devices_characteristic.write("Tt%.0f" % (broadcast.value * 10)))
+            self.add_command(lambda: self.output_characteristic.write("Tt%.0f" % (broadcast.value * 10)))
