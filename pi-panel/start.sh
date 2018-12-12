2af3afceae200bce4f62d39e821456d9e9de80a9
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2018-12-12 09:26:19 -1000

    Turning off the leds

--- pi-panel/start.sh
+++ pi-panel/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Turn off LED
+# from https://www.jeffgeerling.com/blogs/jeff-geerling/controlling-pwr-act-leds-raspberry-pi
+echo none | sudo tee /sys/class/leds/led0/trigger
+echo 1 | sudo tee /sys/class/leds/led0/brightness
+
 SESSION_NAME="pi-panel"
 
 
