825c69255c2a8d26bfdb2ac22d5fa4329490ecc6
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2019-03-14 18:56:08 -0700

    Make beam get battery level

--- VanTomation/server/bean_manager.py
+++ VanTomation/server/bean_manager.py
@@ -23,16 +23,23 @@ class BeanManager(DeviceManager):
 class BeanThread(DeviceThread):
 
     def before_thread(self):
+        BATTERY_SERVICE_UUID = '180F'
+        BATTERY_CHAR_UUID = '2A19'
+
         self.received_uart_data = queue.Queue()
         service_uuid = self.service_and_char_uuids[0][0]
         self.tx_characteristic = self.characteristics[service_uuid][0]
         self.start_notifications(self.tx_characteristic)
         self.next_temp_read = datetime.datetime.now()
+        
+        self.battery_service = self.peripheral.getServiceByUUID(BATTERY_SERVICE_UUID)
+        self.battery_characteristic = self.battery_service.getCharacteristics(BATTERY_CHAR_UUID)[0]
 
 
     def no_data_received(self):
         if datetime.datetime.now() > self.next_temp_read:
             self.next_temp_read = datetime.datetime.now() + datetime.timedelta(seconds=30)
+            # Ask for temperature
             self.add_command(lambda: self.tx_characteristic.write(binascii.unhexlify("a0020020115e6d")))
 
 
@@ -40,6 +47,7 @@ class BeanThread(DeviceThread):
         data_type = ord(data[4])
         if data_type == 0x91:
             temperature = 32.0 + float(ord(data[5])) * 9.0 / 5.0
+            # Now ask for accelerometer
             self.add_command(lambda: self.tx_characteristic.write(binascii.unhexlify("a0020020107f7d")))
             self.add_broadcast(None, "Temperature", temperature)
         elif data_type == 0x90:
@@ -47,5 +55,7 @@ class BeanThread(DeviceThread):
             y = struct.unpack('<h', data[7:9])[0]
             z = struct.unpack('<h', data[9:11])[0]
             self.add_broadcast(None, "Accelerometer", (x, y, z))
+            battery = ord(self.battery_characteristic.read()[0])
+            self.add_broadcast(None, "Battery", battery)
         else:
             logger.error("Unkown type: %d (%s)" % (data_type, binascii.hexlify(data)))
