fa7cc9f9af11413addcddac619b8e1ee551c4652
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2019-05-06 22:39:54 -0700

    Changing io ports

--- VanTomation/server/pi_manager.py
+++ VanTomation/server/pi_manager.py
@@ -8,16 +8,16 @@ class PIManager(SenderReceiver):
     def __init__(self):
         SenderReceiver.__init__(self, "Pi")
         subprocess.call("gpio write 0 0", shell=True)
-        subprocess.call("gpio write 7 0", shell=True)
+        subprocess.call("gpio write 2 0", shell=True)
         subprocess.call("gpio mode 0 out", shell=True)
-        subprocess.call("gpio mode 7 out", shell=True)
+        subprocess.call("gpio mode 2 out", shell=True)
 
 
     def broadcast_received(self, broadcast):
         if broadcast.destination == "Locks" and broadcast.prop == "State":
             logger.debug("Pi received command %s", broadcast.value)
             
-            port = 7 if broadcast.value == "L" else 0
+            port = 2 if broadcast.value == "L" else 0
             subprocess.call("gpio mode %d out" % port, shell=True)
             subprocess.call("gpio write %d 1" % port, shell=True)
             time.sleep(0.3)
