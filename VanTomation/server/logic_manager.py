f3d1f76e5f342474d438ec4b9f2a404f56d03b29
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2019-05-15 22:04:11 -0700

    Properly implemented the logic manager

--- VanTomation/server/logic_manager.py
+++ VanTomation/server/logic_manager.py
@@ -1,20 +1,44 @@
+import datetime
+
 from base import SenderReceiver, logger
 
 
 class LogicManager(SenderReceiver):
     def __init__(self):
-        SenderReceiver.__init__(self, "Logic")
+        SenderReceiver.__init__(self, 'Logic')
         self.current_state = {}
-        self.is_moving = False
+        self.properties = {
+            'Moving': False,
+            'Parked': True,
+        }
+        for (name, value) in self.properties.iteritems():
+            self.add_broadcast(None, name, value)
+        self.stopped_time = 0
+
+
+    def set_property(self, name, value):
+        if name not in self.properties or self.properties[name] != value:
+            self.properties[name] = value
+            self.add_broadcast(None, name, value)
 
 
     def broadcast_received(self, broadcast):
         if broadcast.destination is None:
             self.current_state[broadcast.prop]= broadcast.value
 
-        if broadcast.prop == "Speed" and broadcast.value > 10 and not self.is_moving:
-            self.add_broadcast(None, "Moving", True)
-            self.is_moving = True
-        elif broadcast.prop == "Speed" and broadcast.value < 3 and self.is_moving:
-            self.add_broadcast(None, "Moving", False)
-            self.is_moving = False
+        if broadcast.prop == 'Speed' and broadcast.value > 10 and not self.properties['Moving']:
+            self.set_property('Moving', True)
+            self.set_property('Parked', False)
+            self.stopped_time = 0
+
+        elif broadcast.prop == "Speed" and broadcast.value < 3:
+            self.set_property('Moving', False)
+            if self.stopped_time == 0:
+                self.stopped_time = datetime.datetime.now()
+
+        if not self.properties['Moving'] and self.stopped_time > 0:
+            elapsed = (datetime.datetime.now() - self.stopped_time).seconds
+            if elapsed > 2 * 60:
+                self.set_property('Parked', True)
+                self.stopped_time = 0
+
