e617cb1ac2fa00361206c2335400c0d51d9f0bb2
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2020-06-11 20:43:33 -1000

    organizing by feature a bit

--- homeassistant/install.sh
+++ homeassistant/install.sh
@@ -13,12 +13,19 @@ sudo apt-get upgrade -y
 /home/pi/send-notification.sh "Setup 2 of 4"
 
 sudo apt-get update
-sudo apt-get install -y python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev autoconf mosquitto
+sudo apt-get install -y python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev autoconf
+
 sudo useradd -rm homeassistant -G dialout,gpio,i2c
 sudo mkdir /srv/homeassistant
 sudo chown homeassistant:homeassistant /srv/homeassistant
 
 /home/pi/send-notification.sh "Setup 3 of 4"
+
+# For MQTT
+sudo apt-get install -y mosquitto
+
+# for https://www.home-assistant.io/integrations/nmap_tracker/
+sudo apt-get install -y net-tools nmap
 
 sudo -u homeassistant -H -- bash -c "mkdir /home/homeassistant/.homeassistant && cd /srv/homeassistant && python3 -m venv . && source bin/activate && python3 -m pip install wheel && pip3 install homeassistant"
 
@@ -29,7 +36,7 @@ sudo mv /home/pi/homeassistant/homeassistant.service /etc/systemd/system/home-as
 sudo systemctl --system daemon-reload
 sudo systemctl enable home-assistant@homeassistant
 
-sudo apt-get clean
+sudo apt-get -y clean
 
 # https://raspberrypi.stackexchange.com/a/66939
 
