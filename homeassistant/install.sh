bfd65c4ea4de75f6819b5d47ff66a8a6c5c7dfe5
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2020-06-13 11:13:38 -1000

    adding ha to pi group as well

--- homeassistant/install.sh
+++ homeassistant/install.sh
@@ -15,7 +15,7 @@ sudo apt-get upgrade -y
 sudo apt-get update
 sudo apt-get install -y python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev autoconf
 
-sudo useradd -rm homeassistant -G dialout,gpio,i2c
+sudo useradd -rm homeassistant -G dialout,gpio,i2c,pi
 sudo mkdir /srv/homeassistant
 sudo chown homeassistant:homeassistant /srv/homeassistant
 sudo usermod -a -G homeassistant pi
