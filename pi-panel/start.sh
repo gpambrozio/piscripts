512feacd1891eb779cad843e7422ac9b12fb05f5
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2020-06-13 11:07:27 -1000

    Better screen names

--- pi-panel/start.sh
+++ pi-panel/start.sh
@@ -8,6 +8,6 @@ echo 1 | sudo tee /sys/class/leds/led0/brightness
 SESSION_NAME="pi-panel"
 
 
-screen -S $SESSION_NAME -t myWinName0 -A -d -m
+screen -S $SESSION_NAME -t panelserver -A -d -m
 
-screen -S $SESSION_NAME -p myWinName0 -X stuff $'cd /home/pi/pi-panel && sudo ./panelserver.py\n'
+screen -S $SESSION_NAME -p panelserver -X stuff $'cd /home/pi/pi-panel && sudo ./panelserver.py\n'
