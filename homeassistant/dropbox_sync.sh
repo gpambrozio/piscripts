20f4fc7c96b953be5b1df4f88c68895c928ca855
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2020-06-14 12:23:35 -1000

    Better backup

--- homeassistant/dropbox_sync.sh
+++ homeassistant/dropbox_sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/pi/dropbox_uploader.sh upload /home/homeassistant/.homeassistant homeassistant.conf
+/home/pi/dropbox_uploader.sh -x "__pycache__" -x "deps" -x "home-assistant_v2.db" upload /home/homeassistant/.homeassistant/ homeassistant.conf
