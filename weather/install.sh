306104a72424224dd16de54808be33fc0558c13f
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2021-02-18 11:50:26 -1000

    Setting TZ and adding mqtt extension to weewx

--- weather/install.sh
+++ weather/install.sh
@@ -5,6 +5,9 @@ IFS=$'\n\t'
 
 /home/pi/send-notification.sh "Setup 1 of 4"
 
+# https://raspberrypi.stackexchange.com/a/87185
+sudo timedatectl set-timezone Pacific/Honolulu
+
 # http://weewx.com/docs/debian.htm
 
 sudo apt-get update
@@ -26,11 +29,20 @@ sudo systemctl restart smbd
 # Apache, to serve weewx
 sudo apt-get install -y apache2
 
+# MQTT extension for weewx
+# https://github.com/weewx/weewx/wiki/mqtt
+cd /home/pi
+sudo apt install -y python3-pip
+wget -O weewx-mqtt.zip https://github.com/matthewwall/weewx-mqtt/archive/master.zip
+sudo pip3 install paho-mqtt
+sudo wee_extension --install weewx-mqtt.zip
+sudo rm -f weewx-mqtt.zip
+
 # Cleanup
 
 sudo apt-get -y clean
 
 # https://raspberrypi.stackexchange.com/a/66939
-
 sudo raspi-config nonint do_hostname weather
+
 crontab weather/crontab.txt
