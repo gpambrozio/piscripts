6b46a3eea3788ea08cecfff74e399c0a144dc8d2
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2021-02-17 17:22:20 -1000

    Updating server update script

--- homeassistant/server_update.sh
+++ homeassistant/server_update.sh
@@ -5,6 +5,13 @@ IFS=$'\n\t'
 
 sudo systemctl stop home-assistant@homeassistant
 
-sudo -u homeassistant -H -- bash -c "cd /srv/homeassistant && source bin/activate && python3 -m pip install --upgrade homeassistant"
+sudo -u homeassistant -H -- bash -c "cd /srv/homeassistant_venv_3.8.7 && source bin/activate && python3 -m pip install --upgrade homeassistant"
 
 sudo systemctl start home-assistant@homeassistant
+
+echo "Done."
+echo ""
+echo "To monitor server:"
+echo ""
+echo "journalctl -f -u home-assistant@homeassistant"
+echo ""
