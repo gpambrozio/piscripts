b3a5f2057238277bcb29fb255286d7b01fc0d7d4
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2021-06-30 06:48:59 -1000

    Finished configuring magic mirror

--- pictureframe/install.sh
+++ pictureframe/install.sh
@@ -10,11 +10,36 @@ sudo timedatectl set-timezone Pacific/Honolulu
 
 /home/pi/send-notification.sh "Setup 2 of 4"
 
-# Cleanup
+# picture frame
+# https://www.tomshardware.com/how-to/raspberry-pi-photo-frame
 
-sudo apt-get -y clean
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt install -y nodejs
+git clone https://github.com/MichMich/MagicMirror
+cd MagicMirror
+npm install
+cd modules
+git clone https://github.com/eouia/MMM-GooglePhotos.git
+cd MMM-GooglePhotos
+npm install
+
+SECRET=`cat /boot/googlesecret`
+sudo rm -f /boot/googlesecret
+cat /home/pi/pictureframe/credentials.json | sed "s/CLIENT_SECRET/$SECRET/" > /home/pi/MagicMirror/modules/MMM-GooglePhotos/credentials.json
+
+cp /home/pi/pictureframe/config.js /home/pi/MagicMirror/config/config.js
+cp /home/pi/pictureframe/custom.css /home/pi/MagicMirror/css/custom.css
+
+cd /home/pi
+sudo npm install -g pm2
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi
+chmod +x pictureframe/mm.sh
+pm2 start pictureframe/mm.sh
 
 # https://raspberrypi.stackexchange.com/a/66939
 sudo raspi-config nonint do_hostname pictureframe
+sudo raspi-config nonint do_vnc 0
 
 crontab pictureframe/crontab.txt
+
+/home/pi/send-notification.sh "You need to run cd ~/MagicMirror/modules/MMM-GooglePhotos && node generate_token.js on the desktop"
--- pictureframe/readme.md
+++ pictureframe/readme.md
@@ -1,3 +1,6 @@
 Files with secrets to add before running install.sh:
 
 // From 1password
+(look for pictureframe note)
+
+echo "" > /Volumes/boot/googlesecret
