d1fc5db4e517420d1c0b5761c935aba473a610a7
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2021-06-29 16:35:34 -1000

    Initial picture frame config

---
+++ pictureframe/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+/home/pi/send-notification.sh "Setup 1 of 4"
+
+# https://raspberrypi.stackexchange.com/a/87185
+sudo timedatectl set-timezone Pacific/Honolulu
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+/home/pi/send-notification.sh "Setup 2 of 4"
+
+# Cleanup
+
+sudo apt-get -y clean
+
+# https://raspberrypi.stackexchange.com/a/66939
+sudo raspi-config nonint do_hostname pictureframe
+
+crontab pictureframe/crontab.txt
