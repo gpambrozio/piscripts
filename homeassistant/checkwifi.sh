633bc81b4a375c73ced6c86f177ede4015bdbad0
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2021-07-09 15:01:49 -1000

    Fixing weird host issue

--- homeassistant/checkwifi.sh
+++ homeassistant/checkwifi.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+# Every once in a while the router stops resolving home.local. Doing a ping from the device
+# seems to solve the issue so we do it periodically.
+ping -c1 home.local > /dev/null
+
 # From https://weworkweplay.com/play/rebooting-the-raspberry-pi-when-it-loses-wireless-connection-wifi/
 
 ping -c4 192.168.86.1 > /dev/null
