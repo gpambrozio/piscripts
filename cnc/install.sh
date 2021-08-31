e7f34c0ea3c7dd1eafa2a7521a1a1495a98c1f69
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2021-08-31 08:49:57 -1000

    Saving config

--- cnc/install.sh
+++ cnc/install.sh
@@ -11,12 +11,13 @@ sudo timedatectl set-timezone Pacific/Honolulu
 /home/pi/send-notification.sh "Setup 2 of 4"
 
 # https://github.com/vlachoudis/bCNC
+cp -f /home/pi/cnc/bCNC.config /home/pi/.bCNC
 pip install --upgrade bCNC
 
 # Start automatically
 # Form https://learn.sparkfun.com/tutorials/how-to-run-a-raspberry-pi-program-on-startup#method-2-autostart
 mkdir /home/pi/.config/autostart
-cp cnc/bCNC.desktop /home/pi/.config/autostart/
+cp /home/pi/cnc/bCNC.desktop /home/pi/.config/autostart/
 
 # https://raspberrypi.stackexchange.com/a/66939
 sudo raspi-config nonint do_hostname cnc
