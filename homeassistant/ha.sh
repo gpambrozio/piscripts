473eb1df6ca47148d6fd7c3b9ee076ae4752e3c3
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2021-12-21 18:19:12 -1000

    Trying again... This sucks..

--- homeassistant/ha.sh
+++ homeassistant/ha.sh
@@ -2,17 +2,18 @@
  
 # Based on https://github.com/DubhAd/Home-AssistantConfig/blob/live/local/bin/build_python
 
+set -euo pipefail
+
 PY_VER=${1}
-HA_USER=homeassistant
 VENV_BASE=/srv/homeassistant
 
 python3 -m venv ${VENV_BASE}/venv_${PY_VER}
 source ${VENV_BASE}/venv_${PY_VER}/bin/activate
-pip3 install --upgrade homeassistant
+python3 -m pip install --upgrade homeassistant
 # This installs a bunch of relevant packages automatically, speeding up first startup
 hass --script check_config
 # Now we install requirements
 wget --quiet https://raw.githubusercontent.com/home-assistant/docker/master/requirements.txt -O - | while read LINE
 do
-  pip3 install --upgrade ${LINE}
+  python3 -m pip install --upgrade ${LINE}
 done
--- homeassistant/install.sh
+++ homeassistant/install.sh
@@ -39,8 +39,6 @@ sudo apt-get install -y libssl-dev rustc
 
 # https://itheo.tech/ultimate-python-installation-on-a-raspberry-pi-ubuntu-script/
 sudo bash /home/pi/$INSTALL_NAME/python.sh 3.10.1
-sudo rm /usr/bin/python3
-sudo ln -s /usr/bin/python3.10 /usr/bin/python3
 
 # https://github.com/DubhAd/Home-AssistantConfig/blob/live/local/bin/build_python
 sudo apt-get install -y libopenjp2-7 libtiff-dev
