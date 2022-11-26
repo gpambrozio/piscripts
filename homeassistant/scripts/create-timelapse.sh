13fb4b4cdd9fe7119b135e48a27eb5aa9c359e4f
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2022-11-25 18:38:55 -1000

    Only createw timelapse if there’s enough images

--- homeassistant/scripts/create-timelapse.sh
+++ homeassistant/scripts/create-timelapse.sh
@@ -6,5 +6,9 @@ IN_DIR="/home/homeassistant/.homeassistant/www/cam_captures"
 OUT_DIR="/home/homeassistant/.homeassistant/www/timelapses"
 PREFIX="$1"
 
-ffmpeg -framerate 10 -pattern_type glob -i "${IN_DIR}/${PREFIX}_*.jpg" -c:v libx264 -pix_fmt yuv420p ${OUT_DIR}/${PREFIX}-`date +"%Y-%m-%d-%H-%M"`.mp4
+N=$(ls ${IN_DIR}/${PREFIX}_*.jpg | wc -l)
+if [ $N -gt 10 ] ; then
+  ffmpeg -framerate 10 -pattern_type glob -i "${IN_DIR}/${PREFIX}_*.jpg" -c:v libx264 -pix_fmt yuv420p ${OUT_DIR}/${PREFIX}-`date +"%Y-%m-%d-%H-%M"`.mp4
+fi
+
 rm ${IN_DIR}/${PREFIX}_*.jpg
