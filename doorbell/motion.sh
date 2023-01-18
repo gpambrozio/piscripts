f90c1f2e7dc1b975d1b4a082570291eb1d01ef8d
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2023-01-18 11:13:49 -1000

    Configuring camera to use stream

---
+++ doorbell/motion.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# from https://motion-project.github.io/motion_config.html#basic_setup_picam
+/usr/bin/libcamerify /usr/bin/motion $@
--- doorbell/stream.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-libcamera-vid -t 0 -n --width 800 --height 600 --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream1}' :demux=h264
