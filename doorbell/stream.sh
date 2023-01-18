adb511338cda98ecb4696489f7295efff582133b
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2023-01-18 07:02:31 -1000

    Fixing width and heigth of stream

--- doorbell/stream.sh
+++ doorbell/stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-libcamera-vid -t 0 -n --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream1}' :demux=h264
+libcamera-vid -t 0 -n --width 800 --height 600 --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream1}' :demux=h264
