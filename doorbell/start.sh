c839a2b7d4a9e5c1e810c72f43b1f1b40e0e1cb0
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2023-01-19 06:57:43 -1000

    Trying to keep python alive

--- doorbell/start.sh
+++ doorbell/start.sh
@@ -5,4 +5,4 @@ SESSION_NAME="doorbell"
 screen -S $SESSION_NAME -t ha -A -d -m
 screen -S $SESSION_NAME -X screen -t peripheral
 
-screen -S $SESSION_NAME -p ha -X stuff $'cd /home/pi/doorbell && ./doorbell.py\n'
+screen -S $SESSION_NAME -p ha -X stuff $'cd /home/pi/doorbell && while [ 1 ] ; do ./doorbell.py ; sleep 5 ; done'
