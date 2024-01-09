20836b4d23e5f9378c45d8e334780da73fc32fc9
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2024-01-09 11:01:31 -1000

    Restoring configs

--- tools/install.sh
+++ tools/install.sh
@@ -42,9 +42,11 @@ sudo mv /home/pi/bCNC.config /home/pi/.bCNC
 sudo chown pi:pi /home/pi/.bCNC
 sudo chmod o+w /home/pi/.bCNC
 mkdir /home/pi/gcode/
+
 sudo rclone copy ha:piscripts/$INSTALL_NAME/gcode/ /home/pi/gcode/ --config /home/pi/.config/rclone/rclone.conf
 sudo rclone copy ha:piscripts/$INSTALL_NAME/Desktop/ /home/pi/Desktop/ --config /home/pi/.config/rclone/rclone.conf
 sudo rclone copy ha:piscripts/$INSTALL_NAME/localshare/ /home/pi/.local/share/ --config /home/pi/.config/rclone/rclone.conf
+sudo rclone copy ha:piscripts/$INSTALL_NAME/config/ /home/pi/.config/ --config /home/pi/.config/rclone/rclone.conf
 
 # https://raspberrypi.stackexchange.com/a/66939
 sudo raspi-config nonint do_hostname $INSTALL_NAME
