fbf344e897a1f14f0673cb72b7ff6dba19dd1e1d
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2024-01-09 10:57:31 -1000

    Making it easier to use remotesync

--- remotesync.sh
+++ remotesync.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-if [[ -z "$1" || -z "$2" ]] ; then
-	echo "Usage: $0 machine-name machine-ip"
+if [[ -z "$1" ]] ; then
+	echo "Usage: $0 machine-name [machine-ip]"
 	exit 1
 fi
 
-listcommand="ls -lRT $1"
+MACHINE="$1"
+if [[ -z "$2" ]] ; then
+   MACHINE_IP="$1.local"
+else
+   MACHINE_IP="$2"
+fi
+
+listcommand="ls -lRT ${MACHINE}"
 
 while true ; do
    newfilelist=$( $listcommand )
    if [[ $oldfilelist != $newfilelist ]] ; then
       oldfilelist=$newfilelist
-      rsync --exclude='.*' --exclude='node_modules' --exclude='owm_icons' --exclude='*.pyc' --delete --recursive --perms --itemize-changes --times --compress --human-readable --rsh=ssh "$1/" "pi@$2:/home/pi/$1/"
-      ssh pi@$2 "/home/pi/$1/update.sh"
+      rsync --exclude='.*' --exclude='node_modules' --exclude='owm_icons' --exclude='*.pyc' --delete --recursive --perms --itemize-changes --times --compress --human-readable --rsh=ssh "${MACHINE}/" "pi@${MACHINE_IP}:/home/pi/${MACHINE}/"
+      ssh pi@${MACHINE_IP} "/home/pi/${MACHINE}/update.sh"
    fi
    sleep 1 || exit 2 
 done
