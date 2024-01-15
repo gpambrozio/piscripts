90f4384465defbc8b340a893be75150c3d2ac9ad
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2024-01-14 14:53:56 -1000

    Forgot to update post install

--- tools/update.sh
+++ tools/update.sh
@@ -10,10 +10,10 @@ cp -f /home/pi/$INSTALL_NAME/vlc.desktop /home/pi/.config/autostart/
 sudo cp -f /home/pi/$INSTALL_NAME/smb.conf /etc/samba/
 
 sudo cp /home/pi/$INSTALL_NAME/octoprint.service /etc/systemd/system/octoprint.service
-sudo cp /home/pi/$INSTALL_NAME/webcamd.service /etc/systemd/system/webcamd.service
+sudo cp /home/pi/$INSTALL_NAME/camera-streamer.service /etc/systemd/system/camera-streamer.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable octoprint.service
-sudo systemctl enable webcamd.service
+sudo systemctl enable camera-streamer.service
 
 crontab $INSTALL_NAME/crontab.txt
