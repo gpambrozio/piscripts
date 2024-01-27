3d11c786e83e59263bdd148fccf554dd72858ac9
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2024-01-27 12:18:45 -1000

    Update stream.py with new capture dimensions and add snapshot functionality

--- tools/stream.py
+++ tools/stream.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# From https://github.com/raspberrypi/picamera2/blob/main/examples/mjpeg_server.py
+
 # Mostly copied from https://picamera.readthedocs.io/en/release-1.13/recipes2.html
 # Run this script, then point a web browser at http:<this-ip-address>:8000
 # Note: needs simplejpeg to be installed (pip3 install simplejpeg).
@@ -14,13 +16,16 @@ from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
 
-PAGE = """\
+capture_width = 1296
+capture_height = 972
+
+PAGE = f"""\
 <html>
 <head>
 <title>MJPEG streaming</title>
 </head>
 <body>
-<img src="stream.mjpg" width="640" height="480" />
+<img src="stream.mjpg" width="{capture_width}" height="{capture_height}" />
 </body>
 </html>
 """
@@ -71,6 +76,23 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                 logging.warning(
                     'Removed streaming client %s: %s',
                     self.client_address, str(e))
+        elif self.path == '/snapshot.jpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'image/jpeg')
+            try:
+                with output.condition:
+                    output.condition.wait()
+                    frame = output.frame
+                self.send_header('Content-Length', len(frame))
+                self.end_headers()
+                self.wfile.write(frame)
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
         else:
             self.send_error(404)
             self.end_headers()
@@ -82,12 +104,12 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+picam2.configure(picam2.create_video_configuration(main={"size": (capture_width, capture_height)}))
 output = StreamingOutput()
 picam2.start_recording(JpegEncoder(), FileOutput(output))
 
 try:
-    address = ('', 8000)
+    address = ('', 8090)
     server = StreamingServer(address, StreamingHandler)
     server.serve_forever()
 finally:
